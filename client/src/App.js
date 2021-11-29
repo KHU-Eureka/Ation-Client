89663fd8f059907cff279f3bb654cec65b7fc1d7
LeeSuBin-khu <subini1707@khu.ac.kr>
2021-11-29 18:51:15 +0900

    [chore] 기본 환경 구성

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
-import './App.css';
+import { React } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Insight_Read from "../src/component/insight/Read";
+import Insight_Create from "../src/component/insight/Create";
 
 function App() {
   return (
-    <div className="App">Sensation</div>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/insight" element={<Insight_Read/>} />
+          <Route exact path="/insight/create" element={<Insight_Create/>} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 

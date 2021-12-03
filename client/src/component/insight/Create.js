f0d0ec912e7c70cec9b4db1851739ba1505b2e7e
LeeSuBin-khu <subini1707@khu.ac.kr>
2021-12-03 15:40:31 +0900

    [style] 인사이트-추가-모달창

--- client/src/component/insight/Create.js
+++ client/src/component/insight/Create.js
@@ -1,14 +1,90 @@
-import { React } from "react";
+import { React, useState, useEffect } from "react";
 
-import GNB from "../GNB";
+function Create(props) {
+    // const [pageNum, setPageNum] = useState(1);
+    const [tag, setTag] = useState()
+    const {pageNum, setPageNum, close, header} = props;
 
-function Create() {
-    return (
-        <>
-        <GNB />
-        hello
-        </>
-    );
+    const onNextHandler=() => {
+        if(pageNum < 3) {
+            setPageNum(pageNum+1);
+        } else {
+            setPageNum(3);
+        }
+    }
+
+    const onPrevHandler=() => {
+        if(pageNum > 1) {
+            setPageNum(pageNum-1);
+        } else {
+            setPageNum(1);
+        }
+    }
+
+    
+
+    if(pageNum == 1) {
+        return (
+            <>
+            <div className="page1">
+                <div className="page1-title">인사이트 추가</div>
+                <input className="create-input" placeholder="url을 입력해주세요" />
+                <button className="create-btn" onClick={onNextHandler}>다음</button>
+            </div>
+            </>
+        );
+    } else if(pageNum == 2) {
+        return (
+            <>
+            <div className="page2">
+                <img className="prev" src={header} onClick={onPrevHandler}/>
+                <div className="create-title">카테고리</div>
+                <div className="page2-description">대표 카테고리 한 가지만 선택해주세요</div>
+                <div className="cate1">
+                    <button className="category-btn" style={{width:'81px'}}>디자인</button>
+                    <button className="category-btn" style={{width:'81px'}}>개발</button>
+                    <button className="category-btn" style={{width:'80px'}}>기획</button>
+                </div>
+                <div className="cate2">
+                    <button className="category-btn" style={{width:'127px', marginRight: '6px'}}>뮤직 인사이트</button>
+                    <button className="category-btn" style={{width:'126px'}}>비즈니스 경험담</button>
+                </div>
+                <div className="cate3">
+                    <button className="category-btn" style={{width:'81px'}}>마케팅</button>
+                    <button className="category-btn" style={{width:'81px'}}>Your View</button>
+                    <button className="category-btn" style={{width:'80px'}}>기타</button>
+                </div>
+                <div className="tag-title">연관 태그</div>
+                <div className="tag-container">
+                    <label className="tag-label" for="tag-input">#</label>
+                    <input className="tag-input" value={tag} placeholder="태그를 입력해주세요(최대2개)" />
+                    <button className="tag-btn">추가</button>
+                </div>
+                <button className="create-btn" onClick={onNextHandler} style={{marginTop: '51.5px'}}>다음</button>
+            </div>
+            </>
+        );
+    } else {
+        return (
+            <>
+            <div className="page3">
+                <img className="prev" src={header} onClick={onPrevHandler}/>
+                <div className="complete-title">인사이트 추가 완료!</div>
+                <div className="complete-description">당신의 인사이트가 누군가에겐 큰 영감이 될 거에요:)</div>
+                <div className="filebox">
+                    <input className="upload-file" value="Thumbnail" placeholder="Thumbnail" />
+                    <label for="file">썸네일 이미지 변경</label> 
+                    <input type="file" id="file" />
+                </div>
+                <div style={{height:'29px'}}>
+                    <button className="close" onClick={close} style={{marginRight: '16px'}}>취소</button>
+                    <button className="complete-btn" onClick={close}>완료</button>
+                </div>
+            </div>
+            </>
+        );
+    }
+
 }
 
 export default Create;
--- client/src/component/insight/Read.js
+++ client/src/component/insight/Read.js
@@ -1,11 +1,31 @@
-import { React } from "react";
+import { React, useState, useEffect } from "react";
 
 import GNB from "../GNB";
 import Reco from "./Reco";
 import InsightLNB from "./InsightLNB"
+import Modal from "../modal/Modal";
+import Create from "./Create";
+
+import prev from "../../assets/svg/prev.svg";
+import pin from "../../assets/svg/pin.svg";
+
 import "../../assets/css/insight/Read.css";
 
 function Read() {
+    const [pageNum, setPageNum] = useState(1);
+
+    //modal...
+    const [modalOpen, setModalOpen] = useState(false);
+    const openModal = () => {
+        setModalOpen(true);
+    }
+    const closeModal = () => {
+        setModalOpen(false);
+    }
+    //...modal
+
+    
+
     return (
         <>
         <GNB />
@@ -13,10 +33,21 @@ function Read() {
         <div className="Insight-container">
             <div className="Search-container">
                 <input className="search" placeholder="영감을 얻고 싶은 키워드를 검색해 보세요!"></input>
-                <button className="search-btn">+ Insight</button>
+                <button className="search-btn" onClick={()=> {setPageNum(1); openModal();}}>+ Insight</button>
+                <Modal open={modalOpen}>
+                    <Create pageNum={pageNum} setPageNum={setPageNum} close={closeModal} header={prev}/>
+                </Modal>
             </div>
             <div className="Content-container">
                 <InsightLNB />
+                <div className="img-container">
+                    <ul>
+                        <li><img className="thumbnail-box"></img><img src={pin} style={{position: 'absolute', left: '1011px'}}></img></li>
+                        <li><img className="thumbnail-box"></img></li>
+                        <li><img className="thumbnail-box"></img></li>
+                        <li><img className="thumbnail-box"></img></li>
+                    </ul>
+                </div>
             </div>
         </div>
         </>

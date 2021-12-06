98763a4bf1fff450863e25bd07c2455a7a009298
LeeSuBin-khu <subini1707@khu.ac.kr>
2021-12-06 23:32:57 +0900

    [feat] 인사이트 조회 / 추가 api 연동 및 기능 구현

--- client/src/component/insight/Create.js
+++ client/src/component/insight/Create.js
@@ -1,13 +1,34 @@
 import { React, useState, useEffect } from "react";
+import axios from 'axios';
 
 function Create(props) {
     // const [pageNum, setPageNum] = useState(1);
-    const [tag, setTag] = useState()
     const {pageNum, setPageNum, close, header} = props;
+    const [url, setUrl] = useState("");
+    const [mainCategory, setMainCategory] = useState(0);
+    const [tag, setTag] = useState("");
+    const [hashtag, setHashTag] = useState([]);
+    const [hashtagLength, setHashTagLength] = useState(0);
 
     const onNextHandler=() => {
         if(pageNum < 3) {
             setPageNum(pageNum+1);
+            if(pageNum == 2) {
+                console.log(pageNum);
+                console.log(url);
+                console.log(mainCategory);
+                console.log(hashtag);
+                axios.post('http://163.180.117.22:7218/api/insight', {
+                    "insightMainCategoryId": mainCategory,
+                    "insightSubCategoryId": 1,
+                    "tagList": hashtag,
+                      "url": url
+                }).then(function (response) {
+                    console.log(response);
+                }).catch(function (error) {
+                    console.log(error);
+                  });
+            }
         } else {
             setPageNum(3);
         }
@@ -21,6 +42,46 @@ function Create(props) {
         }
     }
 
+    const urlChangeHandler = (e) => {
+        setUrl(e.target.value);
+    }
+
+    const mainCateClickHandler = (e) => {
+        setMainCategory(e.target.value);
+        console.log(e.target.value)
+    }
+
+    const tagChangeHandler = (e) => {
+        console.log(hashtagLength+"asdf")
+        console.log(hashtag);
+        if(hashtagLength < 2){
+            console.log(e.target.value);
+            setTag(e.target.value);
+        } else {
+            e.preventDefault();
+            alert("해시태그는 최대 2개입니다.")
+        }
+    }
+
+    const tagClickHandler = () => {
+        //e.target.classList.add("clicked");
+        if(hashtagLength < 1){
+            let tag2 = tag.substring(tag.indexOf("#", 0)+1);
+            setHashTag(prev => [...prev, tag2]);
+            setTag(`${tag}   #`);
+        } else if(hashtagLength === 1) {
+            let tag2 = tag.substring(tag.indexOf("#", 0)+1);
+            setHashTag(prev => [...prev, tag2]);
+        } else {
+            alert("해시태그는 최대 2개입니다.");
+        }
+    }
+
+    useEffect(() => {
+        console.log(hashtag);
+        setHashTagLength(hashtag.length);
+        console.log(hashtagLength)
+    }, [hashtag, hashtagLength])
     
 
     if(pageNum == 1) {
@@ -28,7 +89,7 @@ function Create(props) {
             <>
             <div className="page1">
                 <div className="page1-title">인사이트 추가</div>
-                <input className="create-input" placeholder="url을 입력해주세요" />
+                <input className="create-input" value={url} onChange={urlChangeHandler} placeholder="url을 입력해주세요" />
                 <button className="create-btn" onClick={onNextHandler}>다음</button>
             </div>
             </>
@@ -41,24 +102,24 @@ function Create(props) {
                 <div className="create-title">카테고리</div>
                 <div className="page2-description">대표 카테고리 한 가지만 선택해주세요</div>
                 <div className="cate1">
-                    <button className="category-btn" style={{width:'81px'}}>디자인</button>
-                    <button className="category-btn" style={{width:'81px'}}>개발</button>
-                    <button className="category-btn" style={{width:'80px'}}>기획</button>
+                    <button className="category-btn" value={1} onClick={mainCateClickHandler} style={{width:'81px'}}>디자인</button>
+                    <button className="category-btn" value={2} onClick={mainCateClickHandler} style={{width:'81px'}}>개발</button>
+                    <button className="category-btn" value={3} onClick={mainCateClickHandler} style={{width:'80px'}}>기획</button>
                 </div>
                 <div className="cate2">
-                    <button className="category-btn" style={{width:'127px', marginRight: '6px'}}>뮤직 인사이트</button>
-                    <button className="category-btn" style={{width:'126px'}}>비즈니스 경험담</button>
+                    <button className="category-btn" value={4} onClick={mainCateClickHandler} style={{width:'127px', marginRight: '6px'}}>뮤직 인사이트</button>
+                    <button className="category-btn" value={5} onClick={mainCateClickHandler} style={{width:'126px'}}>비즈니스 경험담</button>
                 </div>
                 <div className="cate3">
-                    <button className="category-btn" style={{width:'81px'}}>마케팅</button>
-                    <button className="category-btn" style={{width:'81px'}}>Your View</button>
-                    <button className="category-btn" style={{width:'80px'}}>기타</button>
+                    <button className="category-btn" value={6} onClick={mainCateClickHandler} style={{width:'81px'}}>마케팅</button>
+                    <button className="category-btn" value={7} onClick={mainCateClickHandler} style={{width:'81px'}}>Your View</button>
+                    <button className="category-btn" value={8} onClick={mainCateClickHandler} style={{width:'80px'}}>기타</button>
                 </div>
                 <div className="tag-title">연관 태그</div>
                 <div className="tag-container">
                     <label className="tag-label" for="tag-input">#</label>
-                    <input className="tag-input" value={tag} placeholder="태그를 입력해주세요(최대2개)" />
-                    <button className="tag-btn">추가</button>
+                    <input className="tag-input" value={tag} onChange={tagChangeHandler} placeholder="태그를 입력해주세요(최대2개)" />
+                    <button className="tag-btn" onClick={tagClickHandler}>추가</button>
                 </div>
                 <button className="create-btn" onClick={onNextHandler} style={{marginTop: '51.5px'}}>다음</button>
             </div>
